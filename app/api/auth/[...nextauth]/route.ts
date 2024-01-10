ab0f78b95369d897f4b5cfb4f2ef113a62dc6c47
Luke Morcom <lukemorcom@gmail.com>
2024-01-10 11:27:09 +0000

    uhh this maybe>

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import prismaDb from '../../../../prisma/prismadb';
 
-export const authOptions = {
+const authOptions = {
 	adapter: PrismaAdapter(prismaDb),
 	providers: [
 		GoogleProvider({
