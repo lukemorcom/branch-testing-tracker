5d5d27a4de9e3a0de0b6eb134bf2e15b82caf5a2
Luke Morcom <lukemorcom@gmail.com>
2024-02-04 13:45:09 +0000

    Sort API/APP environments, allow force finish on other persons deployment

--- components/EnvironmentCard.tsx
+++ components/EnvironmentCard.tsx
@@ -1,10 +1,36 @@
+"use client";
+
 import { Callout, Card, Text } from "@tremor/react";
 import { timeAgo } from "../lib/time";
 import { EnvironmentWithDeploymentAndUser } from "../types";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default async function EnvironmentCard({environment, isAuthUserDeployer}: {environment: EnvironmentWithDeploymentAndUser; isAuthUserDeployer: Boolean}) {
+	let hiddenClicks = 0;
+
+	const incrementHiddenClicks = async () => {
+		hiddenClicks++;
+
+		if (hiddenClicks === 5) {
+			if (confirm('You didn\'t deploy this, but you clearly know what you\`re doing. Mark the deployment to ' + environment.name + ' as finished?')) {
+				const res = await axios.post('/api/finish-deployment', {deploymentId: environment.currentDeploymentId});
+
+				if (res.status !== 200) {
+					toast.error('Oops, something went wrong. Harrass Luke about this.');
+		
+					return;
+				}
+		
+				toast.success('You marked ' + environment.name + ' as finished testing.');
+			}
+
+			hiddenClicks = 0;
+		}
+	}
+
 	return (
-		<Card key={environment.id}>
+		<Card key={environment.id} onClick={incrementHiddenClicks}>
 			<div className="flex flex-col h-full">
 				<h3 className="text-4xl font-medium pb-2">{environment.name}</h3>
 				<div className="pb-6">
